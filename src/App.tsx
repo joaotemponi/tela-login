47cac64eed6bc170e349a28c90aaabe9d28c85d8
João Temponi <joao3gamerbr@gmail.com>
2025-03-21 15:27:49 -0300

    codigo feito

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import PHome from './pages/PHome/PHome';
+import PLogin from './pages/PLogin/PLogin';
+
+function App() {
+
+  return (
+    <>
+      <PLogin />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,13 @@
+import estilo from './Cabecalho.module.css';
+import logotipo from '../../assets/logotipo.png';
+
+function Cabecalho() {
+    return (
+        <header className={estilo.cabecalho}>
+            <img src={logotipo} alt="logotipo" />
+            <a href="#">Login</a>
+        </header>
+    );
+}
+
+export default Cabecalho;
